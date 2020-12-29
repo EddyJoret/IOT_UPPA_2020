5f49a1f060996eca361633117ec50b04c0eb56f5
EddyJoret <eddyjoret@hotmail.fr>
2020-12-29 16:38:58 +0100

    Update gestion.js

--- Projet/gestion/gestion.js
+++ Projet/gestion/gestion.js
@@ -7,6 +7,11 @@ function dataParc1(){
 
     document.getElementById('parc3').onclick = function (){dataParc3()};
     document.getElementById('parc3').classList.remove("active_link");
+
+    document.getElementById('h').textContent = "Irrigation de la parcelle 1";
+    document.getElementById('nir').textContent = "2";
+    document.getElementById('vc').textContent = "50mm";
+    document.getElementById('die').textContent = "8j";
 }
 
 function dataParc2(){
@@ -18,6 +23,11 @@ function dataParc2(){
 
     document.getElementById('parc3').onclick = function (){dataParc3()};
     document.getElementById('parc3').classList.remove("active_link");
+
+    document.getElementById('h').textContent = "Irrigation de la parcelle 2";
+    document.getElementById('nir').textContent = "3";
+    document.getElementById('vc').textContent = "65mm";
+    document.getElementById('die').textContent = "5j";
 }
 
 function dataParc3(){
@@ -29,4 +39,9 @@ function dataParc3(){
 
     document.getElementById('parc3').onclick = null;
     document.getElementById('parc3').classList.add("active_link");
+
+    document.getElementById('h').textContent = "Irrigation de la parcelle 3";
+    document.getElementById('nir').textContent = "2";
+    document.getElementById('vc').textContent = "58mm";
+    document.getElementById('die').textContent = "7j";
 }
