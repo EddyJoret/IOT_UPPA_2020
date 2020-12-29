95f7c623200ab554da53424cd9e4a16ffce0987f
EddyJoret <eddyjoret@hotmail.fr>
2020-12-29 15:59:28 +0100

    update dashboard

--- Projet/dashboard/dashboard.js
+++ Projet/dashboard/dashboard.js
@@ -388,19 +388,43 @@ function changeGraph(num){
 }
 
 function dataParc1(){
-    console.log("Parcelle 1");
+    document.getElementById('parc1').onclick = null;
+    document.getElementById('parc1').classList.add("active_link");
+
+    document.getElementById('parc2').onclick = function (){dataParc2()};
+    document.getElementById('parc2').classList.remove("active_link");
+
+    document.getElementById('parc3').onclick = function (){dataParc3()};
+    document.getElementById('parc3').classList.remove("active_link");
+
     displayMoy(1);
     changeGraph(1);
 }
 
 function dataParc2(){
-    console.log("Parcelle 2");
+    document.getElementById('parc1').onclick = function (){dataParc1()};
+    document.getElementById('parc1').classList.remove("active_link");
+
+    document.getElementById('parc2').onclick = null;
+    document.getElementById('parc2').classList.add("active_link");
+
+    document.getElementById('parc3').onclick = function (){dataParc3()};
+    document.getElementById('parc3').classList.remove("active_link");
+
     displayMoy(2);
     changeGraph(2);
 }
 
 function dataParc3(){
-    console.log("Parcelle 3");
+    document.getElementById('parc1').onclick = function (){dataParc1()}
+    document.getElementById('parc1').classList.remove("active_link");
+
+    document.getElementById('parc2').onclick = function (){dataParc2()}
+    document.getElementById('parc2').classList.remove("active_link");
+
+    document.getElementById('parc3').onclick = null;
+    document.getElementById('parc3').classList.add("active_link");
+
     displayMoy(3);
     changeGraph(3);
 }
