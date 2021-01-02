b1a2242f848f744f98fbaa5de09957029f4f59a7
EddyJoret <eddyjoret@hotmail.fr>
2021-01-02 11:07:20 +0100

    update dashboard

--- Projet/dashboard/dashboard.js
+++ Projet/dashboard/dashboard.js
@@ -5,36 +5,36 @@ var sidebarCloseIcon = document.getElementById("sidebarIcon");
 var parc1temp = [];
 var parc1pluie = [];
 var parc1hygro = [];
-var parc1rfu = [];
+var parc1ru = [];
 var parc1et = [];
 var parc1moytemp = 0;
 var parc1moyhygro = 0;
-var parc1moyrfu = 0;
+var parc1moyru = 0;
 var parc1moyet = 0;
 
 var parc2temp = [];
 var parc2pluie = [];
 var parc2hygro = [];
-var parc2rfu = [];
+var parc2ru = [];
 var parc2et = [];
 var parc2moytemp = 0;
 var parc2moyhygro = 0;
-var parc2moyrfu = 0;
+var parc2moyru = 0;
 var parc2moyet = 0;
 
 var parc3temp = [];
 var parc3pluie = [];
 var parc3hygro = [];
-var parc3rfu = [];
+var parc3ru = [];
 var parc3et = [];
 var parc3moytemp = 0;
 var parc3moyhygro = 0;
-var parc3moyrfu = 0;
+var parc3moyru = 0;
 var parc3moyet = 0;
 
 var charttemp;
 var charthygro;
-var chartrfu;
+var chartru;
 var chartet;
 
 var optionstemp = {
@@ -117,9 +117,9 @@ var optionshygro = {
     }
 };
 
-var optionsrfu = {
+var optionsru = {
     series: [{
-      name: 'RFU',
+      name: 'RU',
       data: []
     }],
     chart: {
@@ -134,14 +134,14 @@ var optionsrfu = {
     },
     yaxis: {
         title: {
-            text: 'RFU (en mm)'
+            text: 'RU (en mm)'
         }
     },
     xaxis: {
       type: 'datetime'
     },
     title: {
-        text: 'Capteur RFU'
+        text: 'Capteur RU'
     }
 };
 
@@ -232,7 +232,7 @@ function initData(res, num) {
     var div = 0;
     var moytemp = 0;
     var moyhygro = 0;
-    var moyrfu = 0;
+    var moyru = 0;
     var moyet = 0;
     if(res.feeds.length > 24){
         deb = res.feeds.length - 24;
@@ -243,7 +243,7 @@ function initData(res, num) {
 
         moytemp += parseInt(res.feeds[i].field2, 10);
         moyhygro += parseInt(res.feeds[i].field4, 10);
-        moyrfu += parseInt(res.feeds[i].field5, 10);
+        moyru += parseInt(res.feeds[i].field5, 10);
         moyet += parseInt(res.feeds[i].field3, 10);
 
         var texttemp = '{"x":\"' + res.feeds[i].created_at.substr(0,10) + '-' + res.feeds[i].created_at.substr(-9, 5) + '\","y":\"' + res.feeds[i].field2 + '\"}';
@@ -255,8 +255,8 @@ function initData(res, num) {
         var texthygro = '{"x":\"' + res.feeds[i].created_at.substr(0,10) + '-' + res.feeds[i].created_at.substr(-9, 5) + '\","y":\"' + res.feeds[i].field4 + '\"}';
         var objhygro = JSON.parse(texthygro);
 
-        var textrfu = '{"x":\"' + res.feeds[i].created_at.substr(0,10) + '-' + res.feeds[i].created_at.substr(-9, 5) + '\","y":\"' + res.feeds[i].field5 + '\"}';
-        var objrfu = JSON.parse(textrfu);
+        var textru = '{"x":\"' + res.feeds[i].created_at.substr(0,10) + '-' + res.feeds[i].created_at.substr(-9, 5) + '\","y":\"' + res.feeds[i].field5 + '\"}';
+        var objru = JSON.parse(textru);
 
         var textet = '{"x":\"' + res.feeds[i].created_at.substr(0,10) + '-' + res.feeds[i].created_at.substr(-9, 5) + '\","y":\"' + res.feeds[i].field3 + '\"}';
         var objet = JSON.parse(textet);
@@ -267,7 +267,7 @@ function initData(res, num) {
             parc1temp.push(objtemp);
             parc1pluie.push(objpluie);
             parc1hygro.push(objhygro);
-            parc1rfu.push(objrfu);
+            parc1ru.push(objru);
             parc1et.push(objet);
             break;
 
@@ -275,7 +275,7 @@ function initData(res, num) {
             parc2temp.push(objtemp);
             parc2pluie.push(objpluie);
             parc2hygro.push(objhygro);
-            parc2rfu.push(objrfu);
+            parc2ru.push(objru);
             parc2et.push(objet);
             break;
 
@@ -283,7 +283,7 @@ function initData(res, num) {
             parc3temp.push(objtemp);
             parc3pluie.push(objpluie);
             parc3hygro.push(objhygro);
-            parc3rfu.push(objrfu);
+            parc3ru.push(objru);
             parc3et.push(objet);
             break;
         }
@@ -292,12 +292,12 @@ function initData(res, num) {
     if(div != 0){
       moytemp /= div;
       moyhygro /= div;
-      moyrfu /= div;
+      moyru /= div;
       moyet /= div;
     }else{
       moytemp = 0;
       moyhygro = 0;
-      moyrfu = 0;
+      moyru = 0;
       moyet = 0;
     }
 
@@ -305,21 +305,21 @@ function initData(res, num) {
       case 1:
         parc1moytemp = Math.ceil(moytemp*10)/10;
         parc1moyhygro = Math.ceil(moyhygro*10)/10;
-        parc1moyrfu = Math.ceil(moyrfu*10)/10;
+        parc1moyru = Math.ceil(moyru*10)/10;
         parc1moyet = Math.ceil(moyet*10)/10;
         break;
 
       case 2:
         parc2moytemp = Math.ceil(moytemp*10)/10;
         parc2moyhygro = Math.ceil(moyhygro*10)/10;
-        parc2moyrfu = Math.ceil(moyrfu*10)/10;
+        parc2moyru = Math.ceil(moyru*10)/10;
         parc2moyet = Math.ceil(moyet*10)/10;
         break;
 
       case 3:
         parc3moytemp = Math.ceil(moytemp*10)/10;
         parc3moyhygro = Math.ceil(moyhygro*10)/10;
-        parc3moyrfu = Math.ceil(moyrfu*10)/10;
+        parc3moyru = Math.ceil(moyru*10)/10;
         parc3moyet = Math.ceil(moyet*10)/10;
         break;
     }
@@ -330,21 +330,21 @@ function displayMoy(num) {
       case 1:
         document.getElementById("temp__text").textContent = parc1moytemp + "°";
         document.getElementById("hygro__text").textContent = parc1moyhygro + "%";
-        document.getElementById("rfu__text").textContent = parc1moyrfu + "mm";
+        document.getElementById("ru__text").textContent = parc1moyru + "mm";
         document.getElementById("et__text").textContent = parc1moyet + "%";
         break;
 
       case 2:
         document.getElementById("temp__text").textContent = parc2moytemp + "°";
         document.getElementById("hygro__text").textContent = parc2moyhygro + "%";
-        document.getElementById("rfu__text").textContent = parc2moyrfu + "mm";
+        document.getElementById("ru__text").textContent = parc2moyru + "mm";
         document.getElementById("et__text").textContent = parc2moyet + "%";
         break;
 
       case 3:
         document.getElementById("temp__text").textContent = parc3moytemp + "°";
         document.getElementById("hygro__text").textContent = parc3moyhygro + "%";
-        document.getElementById("rfu__text").textContent = parc3moyrfu + "mm";
+        document.getElementById("ru__text").textContent = parc3moyru + "mm";
         document.getElementById("et__text").textContent = parc3moyet + "%";
         break;
     }
@@ -354,17 +354,17 @@ function displayGraph(num) {
     optionstemp.series[0].data = parc1temp;
     optionshygro.series[1].data = parc1pluie;
     optionshygro.series[0].data = parc1hygro;
-    optionsrfu.series[0].data = parc1rfu;
+    optionsru.series[0].data = parc1ru;
     optionset.series[0].data = parc1et;
 
     charttemp = new ApexCharts(document.querySelector("#apex1"), optionstemp);
     charthygro = new ApexCharts(document.querySelector("#apex2"), optionshygro);
-    chartrfu = new ApexCharts(document.querySelector("#apex3"), optionsrfu);
+    chartru = new ApexCharts(document.querySelector("#apex3"), optionsru);
     chartet = new ApexCharts(document.querySelector("#apex4"), optionset);
 
     charttemp.render();
     charthygro.render();
-    chartrfu.render();
+    chartru.render();
     chartet.render();
 };
 
@@ -374,7 +374,7 @@ function changeGraph(num){
         optionstemp.series[0].data = parc1temp;
         optionshygro.series[1].data = parc1pluie;
         optionshygro.series[0].data = parc1hygro;
-        optionsrfu.series[0].data = parc1rfu;
+        optionsru.series[0].data = parc1ru;
         optionset.series[0].data = parc1et;
         break;
 
@@ -382,7 +382,7 @@ function changeGraph(num){
         optionstemp.series[0].data = parc2temp;
         optionshygro.series[1].data = parc2pluie;
         optionshygro.series[0].data = parc2hygro;
-        optionsrfu.series[0].data = parc2rfu;
+        optionsru.series[0].data = parc2ru;
         optionset.series[0].data = parc2et;
         break;
 
@@ -390,14 +390,14 @@ function changeGraph(num){
         optionstemp.series[0].data = parc3temp;
         optionshygro.series[1].data = parc3pluie;
         optionshygro.series[0].data = parc3hygro;
-        optionsrfu.series[0].data = parc3rfu;
+        optionsru.series[0].data = parc3ru;
         optionset.series[0].data = parc3et;
         break;
     }
 
     charttemp.updateOptions(optionstemp);
     charthygro.updateOptions(optionshygro);
-    chartrfu.updateOptions(optionsrfu);
+    chartru.updateOptions(optionsru);
     chartet.updateOptions(optionset);
 }
 
