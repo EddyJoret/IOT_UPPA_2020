e5610efaa3764a1aa38de6baf3fa89d79cc72801
EddyJoret <eddyjoret@hotmail.fr>
2021-01-15 15:10:37 +0100

    update

--- Projet/capteurs/capteurs.js
+++ Projet/capteurs/capteurs.js
@@ -61,6 +61,39 @@ $(document).ready(function(){
   });
 });
 
+function dataParc1(){
+    document.getElementById('parc1').onclick = null;
+    document.getElementById('parc1').classList.add("active_link");
+
+    document.getElementById('parc2').onclick = function (){dataParc2()};
+    document.getElementById('parc2').classList.remove("active_link");
+
+    document.getElementById('parc3').onclick = function (){dataParc3()};
+    document.getElementById('parc3').classList.remove("active_link");
+}
+
+function dataParc2(){
+    document.getElementById('parc1').onclick = function (){dataParc1()};
+    document.getElementById('parc1').classList.remove("active_link");
+
+    document.getElementById('parc2').onclick = null;
+    document.getElementById('parc2').classList.add("active_link");
+
+    document.getElementById('parc3').onclick = function (){dataParc3()};
+    document.getElementById('parc3').classList.remove("active_link");
+}
+
+function dataParc3(){
+    document.getElementById('parc1').onclick = function (){dataParc1()}
+    document.getElementById('parc1').classList.remove("active_link");
+
+    document.getElementById('parc2').onclick = function (){dataParc2()}
+    document.getElementById('parc2').classList.remove("active_link");
+
+    document.getElementById('parc3').onclick = null;
+    document.getElementById('parc3').classList.add("active_link");
+}
+
 function myFunction() {
    var element = document.body.children[0].children[1];
    var element2 = document.body.children[0].children[0];
